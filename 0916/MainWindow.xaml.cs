b7835c784800a78e663ea35da0926323dac3f0e8
Justin <5b3g0035@stust.edu.tw>
2026-09-16 10:56:06 +0800

    加入專案檔案。

---
+++ 0916/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _0916
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        //登入按鈕
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Password;
+
+            // 帳號密碼
+            string correctUser = "Justin";
+            string correctPass = "20080907";
+
+            // 未輸入帳號密碼
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                txtMessage.Foreground = Brushes.Red;
+                txtMessage.Text = "請輸入帳號與密碼！";
+                return;
+            }
+
+            // 驗證帳密
+            if (username == correctUser && password == correctPass)
+            {
+                txtMessage.Foreground = Brushes.Green;
+                txtMessage.Text = "登入成功！歡迎回來，" + username;
+                Window1 window1 = new Window1();
+                window1.Show();
+                this.Hide();
+
+
+            }
+            else
+            {
+                txtMessage.Foreground = Brushes.Red;
+                txtMessage.Text = "帳號或密碼錯誤，請重新輸入！";
+            }
+        }
+    }
+}
---
+++ 0916/Window1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Xml.Linq;
+
+namespace _0916
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string phone = txtPhone.Text;
+
+            // 顯示彈窗
+            MessageBox.Show($"您輸入的資訊如下：\n姓名：{name}\n電話：{phone}", "提交成功", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
